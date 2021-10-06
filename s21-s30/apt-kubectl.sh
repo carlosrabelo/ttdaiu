60dc5cbf58de754130e58ae22dfa5e5eb4a38607
Carlos Rabelo <carlos.rabelo@ifmt.edu.br>
2021-10-05 20:30:44 -0400

    2021-10-05 20:30

--- s21-s30/apt-kubectl.sh
+++ s21-s30/apt-kubectl.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
