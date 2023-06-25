7a035c1d4b40cbc37f6f0c93a3796b9859ee3885
Carlos Rabelo <developer@carlosrabelo.com.br>
2023-06-24 21:25:54 -0400

    Initial commit

---
+++ focal/roles/bash/files/bash_extras.sh
@@ -0,0 +1,11 @@
+# ~/.bash_extras
+
+#
+export GOROOT="/usr/local/go"
+export GOPATH="$HOME/go"
+
+#
+export PS1='\[\e[0;92m\]\u\[\e[0;97m\]@\[\e[0;92m\]\h \[\e[0;93m\]\w\[\e[0;91m\]$(__git_ps1)\n\[\e[0;32m\]└─\[\e[0m\]$ \[\e[0m\]'
+
+#
+export VAGRANT_DISABLE_VBOXSYMLINKCREATE=1
---
+++ jammy/roles/golang/files/golang.sh
@@ -0,0 +1,5 @@
+#
+
+export GOROOT=/usr/local/go
+export GOPATH=~/
+export PATH=$GOROOT/bin:$PATH
