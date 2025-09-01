f092a8ef8ada1d46abec8f6d58d2d656568543fb
Carlos Rabelo <developer@carlosrabelo.com.br>
2025-09-01 10:08:12 -0400

    2025-09-01 10:08

--- noble/roles/bash/files/bash_extras.sh
+++ noble/roles/bash/files/bash_extras.sh
@@ -1,9 +1,6 @@
 # ~/.bash_extras
 
 #
-export PS1='\[\e[0;92m\]\u\[\e[0;97m\]@\[\e[0;92m\]\h \[\e[0;93m\]\w\[\e[0;91m\]$(__git_ps1)\n\[\e[0;32m\]└─\[\e[0m\]$ \[\e[0m\]'
-
-#
 if [ -d "$HOME/.cargo/bin" ] ; then
     PATH="$HOME/.cargo/bin:$PATH"
 fi
@@ -14,3 +11,6 @@ if [ -d "$HOME/go/bin" ] ; then
     export GOROOT="/usr/local/go"
     export GOPATH="$HOME/go"
 fi
+
+#
+export PS1='\[\e[0;92m\]\u\[\e[0;97m\]@\[\e[0;92m\]\h \[\e[0;93m\]\w\[\e[0;91m\]$(__git_ps1)\n\[\e[0;32m\]└─\[\e[0m\]$ \[\e[0m\]'
